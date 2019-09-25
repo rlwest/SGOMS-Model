c30a0542bf7d94b79f456d1d0c43d2c4bbc78d7f
Rob <rlwest@gmail.com>
2019-09-25 18:54:56 -0400

    works
    listing function works from hot key

--- sgoms.py
+++ sgoms.py
@@ -28,3 +28,9 @@ class Sgoms:
         for pu in self.planning_units:
             print ('planning unit = ', end=' ')
             print (pu)
+            for ut in self.planning_units[pu]:
+                print ('   unit task = ', end=' ')
+                print (ut)
+                for m in self.unit_tasks[ut]:
+                    print ('      method = ', end=' ')
+                    print (m)
